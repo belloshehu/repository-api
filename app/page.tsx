26e2684394be70ddde3a64efde6f6f62734e4e74
Belloshehu <belloshehu1@gmail.com>
2023-06-21 10:45:44 +0100

    Add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,28 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hope page</h1>
+    <div className="text-center flex flex-col gap-6 justify-center items-center lg:w-2/5 w-full mx-auto">
+      <h1 className="text-transparent bg-clip-text bg-gradient-to-bl from-slate-800 to-cyan-600 text-2xl font-semiblod lg:font-extrabold lg:text-4xl">
+        Welcome to reposit
+      </h1>
+
+      <p className="">What are you interested in?</p>
+      <section className="flex justify-around w-full bg-gradient-to-tr flex-col lg:flex-row gap-10 lg:gap-5 from-cyan-300 to-slate-400 p-10 rounded-md shadow-lg">
+        <Link className="" href={"/course"}>
+          <h3 className="text-xl text-transparent underline  bg-clip-text bg-gradient-to-bl from-orange-500 to-cyan-800">
+            Courses
+          </h3>
+          <p>Interesting tech courses</p>
+        </Link>
+        <Link className="" href={"/code/repos"}>
+          <h3 className="text-xl text-transparent underline bg-clip-text bg-gradient-to-bl from-orange-500 to-cyan-800">
+            Code{" "}
+          </h3>
+          <p>Beautifully developed codes</p>
+        </Link>
+      </section>
     </div>
   );
 }
